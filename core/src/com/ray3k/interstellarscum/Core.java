65721e809d9ea1c26c22bdb922394332bf42850b
Raeleus <raymond.ray3k@gmail.com>
2019-06-24 00:32:24 -0700

    added asset manager setup basic game.

--- core/src/com/ray3k/interstellarscum/Core.java
+++ core/src/com/ray3k/interstellarscum/Core.java
@@ -1,35 +1,82 @@
 package com.ray3k.interstellarscum;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.esotericsoftware.spine.*;
 import com.rafaskoberg.gdx.typinglabel.*;
 
-public class Core extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Core extends Game {
+	public static Core core;
+	public static AssetManager assetManager;
+	public static Skin skin;
+	public static TextureAtlas textureAtlas;
+	public static SkeletonRenderer skeletonRenderer;
+	public static float sfxVolume;
+	public static float bgmVolume;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//set variables
+		core = this;
+		assetManager = new AssetManager(new InternalFileHandleResolver());
+		assetManager.setLoader(SkeletonData.class, new SkeletonDataLoader(assetManager.getFileHandleResolver()));
+		skeletonRenderer = new SkeletonRenderer();
+		skeletonRenderer.setPremultipliedAlpha(false);
+		sfxVolume = 1.0f;
+		bgmVolume = 1.0f;
+		
+		//load assets
+		assetManager.load("skin/interstellar-scum.json", Skin.class);
+		
+		SkeletonDataLoader.SkeletonDataLoaderParameter parameter = new SkeletonDataLoader.SkeletonDataLoaderParameter("skin/interstellar-scum.atlas");
+		assetManager.load("spine/intro.json", SkeletonData.class, parameter);
+		assetManager.load("spine/intro.json", SkeletonData.class, parameter);
+		
+		assetManager.load("sfx/blast.mp3", Sound.class);
+		assetManager.load("sfx/concede.mp3", Sound.class);
+		assetManager.load("sfx/confirm.mp3", Sound.class);
+		assetManager.load("sfx/fart.mp3", Sound.class);
+		assetManager.load("sfx/hibernate.mp3", Sound.class);
+		assetManager.load("sfx/vomit.mp3", Sound.class);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.dispose();
+		
+		assetManager.dispose();
+	}
+	
+	public Sound playSound(String name, float volume) {
+		Sound sound = assetManager.get("sfx/" + name + ".mp3");
+		sound.play(sfxVolume * volume);
+		return sound;
+	}
+	
+	public Music playMusic(String name, float volume) {
+		Music music = assetManager.get("bgm/" + name + ".mp3");
+		music.setVolume(bgmVolume * volume);
+		music.play();
+		return music;
 	}
 }
