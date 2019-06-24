8a1f14665b9de840612bb0c57214dfc2f5fc2896
Raeleus <raymond.ray3k@gmail.com>
2019-06-23 23:48:06 -0700

    Added typing label and spine libs.

--- core/src/com/ray3k/interstellarscum/Core.java
+++ core/src/com/ray3k/interstellarscum/Core.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.esotericsoftware.spine.*;
+import com.rafaskoberg.gdx.typinglabel.*;
 
 public class Core extends ApplicationAdapter {
 	SpriteBatch batch;
