b7ca317605347508682a93c69ccce4837974b53b
Raeleus <raymond.ray3k@gmail.com>
2019-06-23 21:29:02 -0700

    configured resolution and index.html

--- desktop/src/com/ray3k/interstellarscum/desktop/DesktopLauncher.java
+++ desktop/src/com/ray3k/interstellarscum/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.ray3k.interstellarscum.Core;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 576;
+		config.resizable = false;
 		new LwjglApplication(new Core(), config);
 	}
 }
