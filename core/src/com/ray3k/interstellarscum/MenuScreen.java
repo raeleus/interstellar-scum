8e7f955ec9c69e06c751ba48a8a9978a1605176a
Raeleus <raymond.ray3k@gmail.com>
2019-06-25 02:41:26 -0700

    Improved functionality of menu.

--- core/src/com/ray3k/interstellarscum/MenuScreen.java
+++ core/src/com/ray3k/interstellarscum/MenuScreen.java
@@ -1,10 +1,14 @@
 package com.ray3k.interstellarscum;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
@@ -36,9 +40,19 @@ public class MenuScreen implements Screen {
         
         root.row();
         final TextField textField = new TextField("",skin);
+        textField.setName("textField");
         textField.setAlignment(Align.center);
         root.add(textField).spaceBottom(75).minWidth(300);
         stage.setKeyboardFocus(textField);
+        textField.addListener(new InputListener() {
+            @Override
+            public boolean keyDown(InputEvent event, int keycode) {
+                if (keycode == Input.Keys.ENTER) {
+                    nextScreen();
+                }
+                return super.keyDown(event, keycode);
+            }
+        });
         
         root.row();
         TextButton textButton = new TextButton("OK", skin);
@@ -46,28 +60,41 @@ public class MenuScreen implements Screen {
         textButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                Core.infectedCrew = new Array<String>();
-                Core.stasisCrew = new Array<String>();
-                Core.normalCrew = new Array<String>();
-                
-                String name = textField.getText();
-                if (name.equals("")) name = "You";
-                Core.normalCrew.add(name);
-                
-                Array<String> temp = new Array<String>(Core.names);
-                temp.shuffle();
-                for (int i = 0; i < 15; i++) {
-                    Core.normalCrew.add(temp.pop());
-                }
-                
-                Gdx.input.setInputProcessor(null);
-                Core.core.setScreen(new IntroScreen());
+                nextScreen();
             }
         });
         
         root.row();
         label = new Label("A game by Raeleus... Copyright © Raymond Buckley 2019", skin);
         root.add(label).expand().bottom();
+        
+        stage.getRoot().setColor(1, 1, 1, 0);
+        stage.addAction(Actions.fadeIn(1));
+    }
+    
+    private void nextScreen() {
+        Core.infectedCrew = new Array<String>();
+        Core.stasisCrew = new Array<String>();
+        Core.normalCrew = new Array<String>();
+    
+        TextField textField = stage.getRoot().findActor("textField");
+        String name = textField.getText();
+        if (name.equals("")) name = "You";
+        Core.normalCrew.add(name);
+    
+        Array<String> temp = new Array<String>(Core.names);
+        temp.shuffle();
+        for (int i = 0; i < 15; i++) {
+            Core.normalCrew.add(temp.pop());
+        }
+    
+        Gdx.input.setInputProcessor(null);
+        stage.addAction(Actions.sequence(Actions.fadeOut(1), Actions.delay(1), new SingleAction() {
+            @Override
+            public void perform() {
+                Core.core.setScreen(new IntroScreen());
+            }
+        }));
     }
     
     @Override
